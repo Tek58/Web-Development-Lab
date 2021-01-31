7210224824ed873b10c35330d75009460b4d8127
Taklemariam Alazar <teklealazar399@gmail.com>
2021-02-01 00:24:02 +0300

    filter updated and date sort tried

--- TaskManager/assets/js/app.js
+++ TaskManager/assets/js/app.js
@@ -5,11 +5,63 @@ const filter = document.querySelector('#filter');
 const taskList = document.querySelector('.collection'); 
 const clearBtn = document.querySelector('.clear-tasks'); 
 
+// ***********************************************************************
+const aUpToz = document.querySelector('A_Z');
+const zUpToa = document.querySelector('Z_A');
+// ***********************************************************************
+
+
 form.addEventListener('submit', addNewTask);
 clearBtn.addEventListener('click', clearAllTasks);
 filter.addEventListener('keyup', filterTasks);
 taskList.addEventListener('click', removeTask);
 reloadIcon.addEventListener('click', reloadPage);
+
+// ***********************************************************************
+aUpToz.addEventListener('click', filterAuptoZ);
+zUpToa.addEventListener('click', filterZuptoA);
+
+// ***********************************************************************
+function filterAuptoZ(){
+    var txtValue, ul, li, a; 
+    ul = document.getElementById("myUl");
+    li = ul.getElementsByTagName('li');
+    var lis  = []
+    for (i = 0; i < li.length; i++) {
+        a = li[i].firstChild;
+        txtValue = a.textContent;
+        lis.push(textValue)
+      }
+
+    var sortedLis =  lis.sort(function (a, b) {var dateA = new Date(a.date), dateB = new Date(b.date)
+        return dateA - dateB
+    });
+    for (i = 0; i < li.length; i++) {
+        a = li[i].firstChild;
+        a = sortedLis[i] 
+      }
+}
+
+function filterZuptoA(e){
+    var txtValue, ul, li, a; 
+    ul = document.getElementById("myUl");
+    li = ul.getElementsByTagName('li');
+    var lis  = []
+    for (i = 0; i < li.length; i++) {
+        a = li[i].firstChild;
+        txtValue = a.textContent;
+        lis.push(textValue)
+      }
+
+    var sortedLis =  lis.sort(function (a, b) {var dateA = new Date(a.date), dateB = new Date(b.date)
+        return dateB - dateA
+    });
+    for (i = 0; i < li.length; i++) {
+        a = li[i].firstChild;
+        a = sortedLis[i] 
+      }
+}
+// ****************************************************************************************************
 
 
 function addNewTask(e){
@@ -53,6 +105,8 @@ function filterTasks(e) {
         }
       }
 } 
+// *************************************************************************************
+
 
 // *************************************************************************************
 
@@ -67,3 +121,7 @@ function removeTask(e){
 function reloadPage() {
     location.reload();
 }
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, options);
+//   });
